ca4d83be37a4da1e5e4cbcd91a4695abfc5b6e4c
utsugi <elvinrandall052@gmail.com>
2026-05-09 20:55:55 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<div    ><h1>个人简介</h1>
+        <p>头像：</p>
+        <img src="1.jpg" width="150" height="100" alt=""></div>
+     <p>我是xxx，今年xx岁，来自xxx。</p>   
+   <br>
+    <a class="简历" href="demo.html" target="_top">简历</a>
